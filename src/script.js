610beb32e00fba58f25b3c420fdd303890db005f
Filipe Daineze <jimmyfilips@gmail.com>
2020-06-03 21:50:54 -0300

    Atualização, view diferente pra cada velocidade

--- src/script.js
+++ src/script.js
@@ -5,30 +5,30 @@ function getHeader() {
     if (header) {
         clearInterval(interval);
 
-        createButton(header, 1.5, "timesButtonOne");
-        createButton(header, 2, "timesButtonTwo");
+        createButton(header, 1.5, "timesButtonOne", "cadetblue");
+        createButton(header, 2, "timesButtonTwo", "green");
 
         //Aproveita pra aplicar o tema dark
         document.body.className = 'web dark';
     }
 }
 
-function createButton(header, speed, cname) {
+function createButton(header, speed, cname, color) {
     const btn = document.createElement('button');
     btn.innerHTML = speed + "x";
     btn.classList.add(cname);
     header.appendChild(btn);
-    createListener(btn, speed);
+    createListener(btn, speed, color);
 }
 
-function createListener(btn, x) {
+function createListener(btn, x, color) {
     btn.addEventListener("click", () => {
-
         const audios = document.querySelectorAll("audio");
         audios.forEach((audio) => {
             audio.playbackRate = x;
             let view = audio.closest("div");
             view.classList.add('accelered');
+            view.style.borderColor = color;
         })
     })
 }
