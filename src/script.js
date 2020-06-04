b4982714a35624d1f68ee8422c0e439a6032d003
Filipe Daineze <jimmyfilips@gmail.com>
2020-06-03 21:00:14 -0300

    Identificação visual adicionada
    Identificação visual adicionada a audios no qual o efeito está aplicado

--- src/script.js
+++ src/script.js
@@ -1,20 +1,20 @@
 const interval = setInterval(getHeader, 1000)
 
 function getHeader() {
-    const header = document.querySelector("._1QUKR");
+    const header = document.querySelector("#side > header");
     if (header) {
         clearInterval(interval);
 
-        createButton(header, "buttonOne", 1.5, "timesButtonOne");
-        createButton(header, "buttonTwo", 2, "timesButtonTwo");
+        createButton(header, 1.5, "timesButtonOne");
+        createButton(header, 2, "timesButtonTwo");
 
         //Aproveita pra aplicar o tema dark
         document.body.className = 'web dark';
     }
 }
 
-function createButton(header, name, speed, cname) {
-    const btn = document.createElement(name);
+function createButton(header, speed, cname) {
+    const btn = document.createElement('button');
     btn.innerHTML = speed + "x";
     btn.classList.add(cname);
     header.appendChild(btn);
@@ -23,9 +23,12 @@ function createButton(header, name, speed, cname) {
 
 function createListener(btn, x) {
     btn.addEventListener("click", () => {
+
         const audios = document.querySelectorAll("audio");
         audios.forEach((audio) => {
             audio.playbackRate = x;
+            let view = audio.closest("div");
+            view.classList.add('accelered');
         })
     })
 }
